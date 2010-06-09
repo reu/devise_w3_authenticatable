48b9f7da047252238c9f46fe4b83ba5f45ae11d2
reu <rnavarro1@gmail.com>
2010-06-09 00:09:20 -0300

    And another wrong require

--- lib/devise_w3_authenticatable.rb
+++ lib/devise_w3_authenticatable.rb
@@ -2,7 +2,7 @@
 require 'devise'
 
 require 'devise_w3_authenticatable/schema'
-require 'devise_w3_authenticatable/ldap_adapter'
+require 'devise_w3_authenticatable/w3_adapter'
 require 'devise_w3_authenticatable/routes'
 
 module Devise
@@ -12,7 +12,6 @@ module Devise
 end
 
 # Add w3_authenticatable strategy to defaults.
-#
 Devise.add_module(:w3_authenticatable,
                   :strategy   => true,
                   :controller => :sessions,
