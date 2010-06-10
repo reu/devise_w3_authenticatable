654f1dc5f73093a784506c94b6845fd2226c8812
Rodrigo Navarro <rnavarro1@gmail.com>
2010-06-10 16:47:27 -0300

    Fix routes generation

--- lib/devise_w3_authenticatable.rb
+++ lib/devise_w3_authenticatable.rb
@@ -5,14 +5,9 @@ require 'devise_w3_authenticatable/schema'
 require 'devise_w3_authenticatable/w3_adapter'
 require 'devise_w3_authenticatable/routes'
 
-module Devise
-  # Attribute for login
-  mattr_accessor :w3_login_attribute
-  @@w3_login_attribute = nil
-end
-
 # Add w3_authenticatable strategy to defaults.
 Devise.add_module(:w3_authenticatable,
                   :strategy   => true,
                   :controller => :sessions,
+                  :route => :w3_authenticatable,
                   :model  => 'devise_w3_authenticatable/model')
