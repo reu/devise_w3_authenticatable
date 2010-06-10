c8bf1969e613d9923d158d9e6bd02402266ae3b7
Rodrigo Navarro <rnavarro1@gmail.com>
2010-06-10 16:46:01 -0300

    Use bluepages to retrieve user CN

--- lib/devise_w3_authenticatable/w3_adapter.rb
+++ lib/devise_w3_authenticatable/w3_adapter.rb
@@ -1,11 +1,27 @@
 require 'net/ldap'
+require 'open-uri'
+require 'hpricot'
 
 module Devise
   module W3Adapter
     def self.valid_credentials?(login, password)
+      return unless dn = get_dn_from_bluepages(login)
+
       user = Net::LDAP.new :host => "bluepages.ibm.com"
-      user.authenticate login, password
+      user.authenticate(dn, password)
       user.bind
+    end
+
+    protected
+
+    def self.get_dn_from_bluepages(login)
+      begin
+        bluepages_url = "http://bluepages.ibm.com/BpHttpApisv3/slaphapi?ibmperson/mail=#{login}.list/byxml?cn"
+        ibmer = Hpricot.XML open(bluepages_url)
+        (ibmer/:"entry").first.attributes["dn"]
+      rescue
+        nil
+      end
     end
   end
 end
