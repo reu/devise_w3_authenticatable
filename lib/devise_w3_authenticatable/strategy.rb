9e050eaaf7ee26f17f40b3f58bd737bb4a454a43
Rodrigo Navarro <rnavarro1@gmail.com>
2010-06-11 15:15:42 -0300

    Fix syntax

--- lib/devise_w3_authenticatable/strategy.rb
+++ lib/devise_w3_authenticatable/strategy.rb
@@ -6,7 +6,7 @@ module Devise
       def authenticate!
         resource = mapping.to.find_for_w3_authentication(authentication_hash)
 
-        if validate resource { resource.valid_w3_password? password }
+        if validate(resource) { resource.valid_w3_password?(password) }
           resource.after_w3_authentication
           success! resource
         else
