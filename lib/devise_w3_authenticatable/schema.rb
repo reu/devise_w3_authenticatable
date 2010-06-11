315a7d6eac664a1c6f6f3e3308282184a20f6446
Rodrigo Navarro <rnavarro1@gmail.com>
2010-06-11 11:25:11 -0300

    Allow defaults

--- lib/devise_w3_authenticatable/schema.rb
+++ lib/devise_w3_authenticatable/schema.rb
@@ -1,7 +1,8 @@
 Devise::Schema.class_eval do
   def w3_authenticatable(options={})
     null = options[:null] || false
+    default = options[:default] || ""
 
-    apply_schema :logon, String, :null => null
+    apply_schema :logon, String, :null => null, :default => default
   end
 end
