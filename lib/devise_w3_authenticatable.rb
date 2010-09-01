6163780d4f76730a238c20e84850cd223a97f670
Rodrigo Navarro <rnavarro1@gmail.com>
2010-09-01 11:26:27 -0300

    Requiring active support

--- lib/devise_w3_authenticatable.rb
+++ lib/devise_w3_authenticatable.rb
@@ -4,6 +4,8 @@ require 'devise'
 require 'devise_w3_authenticatable/schema'
 require 'devise_w3_authenticatable/w3_adapter'
 
+require 'active_support'
+
 # Add w3_authenticatable strategy to defaults.
 Devise.add_module(:w3_authenticatable,
                   :strategy   => true,
