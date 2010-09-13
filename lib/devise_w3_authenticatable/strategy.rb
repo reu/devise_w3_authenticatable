ecd2b11946e5a580814d21215d1186f2d4e702d3
Rodrigo Navarro <rnavarro1@gmail.com>
2010-09-13 10:11:57 -0300

    Save resource when auto create is on

--- lib/devise_w3_authenticatable/strategy.rb
+++ lib/devise_w3_authenticatable/strategy.rb
@@ -10,6 +10,7 @@ module Devise
 
         if validate(resource) { employee = resource.authenticate_with_w3!(password) }
           resource.after_w3_authentication employee
+          resource.save if DeviseW3Authenticatable.auto_create_user
           success! resource
         else
           fail :invalid
