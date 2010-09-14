cc2dd873c2dd47bbd641937d3f1b147b09150392
Rodrigo Navarro <rnavarro1@gmail.com>
2010-09-14 09:29:59 -0300

    Getting rid of local variable

--- lib/devise_w3_authenticatable/strategy.rb
+++ lib/devise_w3_authenticatable/strategy.rb
@@ -6,16 +6,18 @@ module Devise
       def authenticate!
         resource = mapping.to.find_for_w3_authentication(authentication_hash)
 
-        employee = nil
-
-        if validate(resource) { employee = resource.authenticate_with_w3!(password) }
-          resource.after_w3_authentication employee
+        if validate(resource) { bluepages_information = resource.authenticate_with_w3!(password) }
+          resource.after_w3_authentication bluepages_information
           resource.save if DeviseW3Authenticatable.auto_create_user
           success! resource
         else
           fail :invalid
         end
       end
+
+      private
+
+        attr_accessor :bluepages_information
     end
   end
 end
